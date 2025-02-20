3546a39f24776723f47aed2027da1800df6997b0
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2025-02-20 18:34:47 +0100

    implement publish and listing all events for only one stream

--- eventstore.go
+++ eventstore.go
@@ -7,17 +7,43 @@ import (
 	"os"
 )
 
-type Repository struct {
-	conn *pgx.Conn
+type EventStore struct {
+	connection *pgx.Conn
 }
 
-func NewEventStore(ctx context.Context, connStr string) (*Repository, error) {
+func (e EventStore) Publish(bytes []byte) error {
+	_, err := e.connection.Exec(context.Background(), "insert into events values ($1)", bytes)
+	return err
+}
+
+func (e EventStore) All() ([][]byte, error) {
+	rows, err := e.connection.Query(context.Background(), "select * from events")
+	if err != nil {
+		return nil, err
+	}
+	output := make([][]byte, 0)
+	for rows.Next() {
+		var current []byte
+		rowErr := rows.Scan(&current)
+		if rowErr != nil {
+			return nil, rowErr
+		}
+		output = append(output, current)
+	}
+	return output, nil
+}
+
+func NewEventStore(ctx context.Context, connStr string) (*EventStore, error) {
 	conn, err := pgx.Connect(ctx, connStr)
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		return nil, err
 	}
-	return &Repository{
-		conn: conn,
+	_, err = conn.Exec(context.Background(), "create table events (events jsonb)")
+	if err != nil {
+		return nil, err
+	}
+	return &EventStore{
+		connection: conn,
 	}, nil
 }
--- eventstore_test.go
+++ eventstore_test.go
@@ -8,12 +8,17 @@ import (
 	"testing"
 )
 
-func TestPublish(t *testing.T) {
+func TestPublish_and_list_all_events(t *testing.T) {
 	postgresContainer, err := runTestContainer()
 	require.NoError(t, err)
 	defer postgresContainer.Cancel()
 	es, err := ges.NewEventStore(context.Background(), postgresContainer.ConnectionString(t))
 	require.NoError(t, err)
 
-	assert.NotEmpty(t, es)
+	require.NoError(t, es.Publish([]byte(`"my_event_data"`)))
+	events, err := es.All()
+	require.NoError(t, err)
+
+	assert.Len(t, events, 1)
+	assert.Equal(t, []byte(`"my_event_data"`), events[0])
 }
