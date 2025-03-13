7d6c51ee050b36faa724c67c33718717086300a3
Nicolas Barbey <nicolas.barbey@adevinta.com>
2025-03-13 15:25:38 +0100

    r remove dead code

--- eventstore/codec/codec_versionned.go
+++ eventstore/codec/codec_versionned.go
@@ -16,28 +16,3 @@ func (v *Versioned[E]) UnmarshallWithVersion(version string, payload []byte) (ev
 	}
 	return event, err
 }
-
-func (v *Versioned[E]) UnmarshallWithTypeAndVersion(version string, typeHint string, payload []byte) (event E, err error) {
-	event, err = v.TypedCodec.UnmarshallWithType(typeHint, payload)
-	versioned, ok := any(&event).(VersionSetter)
-	if ok {
-		versioned.SetVersion(version)
-	}
-	return event, err
-}
-
-type VersionedTypedUnmarshaller[E any] interface {
-	UnmarshallWithTypeAndVersion(version string, typeHint string, payload []byte) (event E, err error)
-}
-
-func UnmarshallAllWithTypeAndVersions[E any](u VersionedTypedUnmarshaller[E], types []string, versions []string, payloads [][]byte) (events []E, err error) {
-	output := make([]E, 0)
-	for i, payload := range payloads {
-		event, err := u.UnmarshallWithTypeAndVersion(versions[i], types[i], payload)
-		if err != nil {
-			return nil, err
-		}
-		output = append(output, event)
-	}
-	return output, nil
-}
