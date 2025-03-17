6103beb6b42eae6c6da3b3ccf815898e878fe914
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2025-03-17 17:34:02 +0100

    t add some bench for event store with different codecs

--- eventstore/eventstore_benchmark_test.go
+++ eventstore/eventstore_benchmark_test.go
@@ -21,6 +21,36 @@ func BenchmarkPublisher(b *testing.B) {
 	}
 }
 
+type benchEvent struct {
+	Benchmark string
+	ID        uint
+	BoolArray []bool
+}
+
+func BenchmarkPublisherJSON(b *testing.B) {
+	typedRepository, err := repository.NewTypedRepository[benchEvent](context2.Background(), postgresContainer.ConnectionString("search_path=string_events"), codec.NewJSONCodecWithTypeHints[benchEvent](nil))
+	if err != nil {
+		panic(err)
+	}
+	publisher := eventstore.NewPublisher[benchEvent]("my_stream", typedRepository)
+
+	for i := 0; i < b.N; i++ {
+		_ = publisher.Publish(context2.Background(), benchEvent{Benchmark: "awesome bench", ID: 1, BoolArray: []bool{true, true, false, true, false, false}})
+	}
+}
+
+func BenchmarkPublisherGob(b *testing.B) {
+	typedRepository, err := repository.NewTypedRepository[benchEvent](context2.Background(), postgresContainer.ConnectionString("search_path=string_events"), codec.NewGobCodecWithTypeHints[benchEvent](nil))
+	if err != nil {
+		panic(err)
+	}
+	publisher := eventstore.NewPublisher[benchEvent]("my_stream", typedRepository)
+
+	for i := 0; i < b.N; i++ {
+		_ = publisher.Publish(context2.Background(), benchEvent{Benchmark: "awesome bench", ID: 1, BoolArray: []bool{true, true, false, true, false, false}})
+	}
+}
+
 func BenchmarkSubscriber(b *testing.B) {
 	es, err := eventstore.NewEventStore[string](context2.Background(), postgresContainer.ConnectionString("search_path=string_events"))
 	if err != nil {
